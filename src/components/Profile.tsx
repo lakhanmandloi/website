8a8c25237f5370f24cb1b978e31ec0338d29d6fd
Lakhan Mandloi <lakhansinghmandloi1@gmail.com>
2023-10-02 10:41:18 +0530

    update

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,4 +1,4 @@
-export default function Summary() {
+export default function Profile() {
   return (
     <section className="rounded-xl bg-white p-6 shadow print:p-0 print:shadow-none">
       <h3 className="mb-4 border-b-2 border-gray-100 pb-1 text-lg font-semibold print:mb-2 print:border-b print:text-sm">
